8d73cfb7c09bf54c430e9d5f1be0d0ec77802504
vivekkumar1994 <96063498+vivekkumar1994@users.noreply.github.com>
2022-04-01 19:20:03 +0530

    change something
    changed something

---
+++ component/navbar.js
@@ -0,0 +1,25 @@
+function navbar() {
+    return ` <div class="navbar">
+    <div>
+      <img
+        src="https://www.revv.co.in/grapheneImages/newopen/logo.svg"
+        alt=""
+      />
+    </div>
+    <div class="rnav">
+      <div class="fbod">Hyundai- <span>subscription</span></div>
+      <div class="fbod">Mahindra- <span>subscription</span></div>
+      <div class="fbo"><a href="faq.html">FAQs</a></div>
+      <div class="fbo">
+      <span> <a href="login.html">login or</a></span>
+     
+      <span> <a href="signup.html"> Signup</a></span>
+    
+
+      
+      </div>
+    </div>
+  </div>`;
+  }
+  
+  export default navbar;
